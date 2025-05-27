a627a4d0cd01c6f67c46ca0382be51fdeb6a30b8
Igor Cuckovic <cuckovic@gmail.com>
2025-05-27 11:33:17 +0200

    first

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// import { SciChartSurface } from "scichart";
+import ChartExample from "./ChartExample";
+
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex flex-column min-h-screen justify-center items-center">
+        <ChartExample />
+      </div>
+    </main>
+  );
+}
